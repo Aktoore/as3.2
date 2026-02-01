0c1e0ee5da181b412b175d9e1147f95437fb2465
Aktore <aktore2019@gmail.com>
2026-02-02 00:07:45 +0500

    Fix mongo connection

--- database/mongo.js
+++ database/mongo.js
@@ -1,19 +1,15 @@
 const { MongoClient } = require("mongodb");
 
-const uri = process.env.MONGO_URI;
+console.log("ENV MONGO_URI =", process.env.MONGO_URI);
 
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
 
 let db;
 
 async function connectDB() {
   if (!db) {
     await client.connect();
-
-    // ЯВНО указываем БД
     db = client.db("crypto_watcher");
 
     console.log("Connected to DB:", db.databaseName);
