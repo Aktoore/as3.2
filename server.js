ce5168dc9bfb128b78f96bd6777390ef71fa704d
Aktore <aktore2019@gmail.com>
2026-02-01 22:34:39 +0500

    Fix: install dotenv

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
-require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
